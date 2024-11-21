bc3fff7ead9123cba80d2feee6f564053ac60144
Darshan L <darshanl1254@gmail.com>
2024-11-21 23:36:07 +0530

    To Print unique characters in a given string

---
+++ strings/unique_string.java
@@ -0,0 +1,24 @@
+package strings;
+
+import java.util.Scanner;
+
+public class unique_string {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String to check unique charcaters:");
+		String str = sc.nextLine();
+
+		for (int i = 0; i < str.length(); i++) {
+			boolean flag = true;
+			for (int j = 0; j < str.length(); j++) {
+				if (i != j && str.charAt(i) == str.charAt(j)) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) {
+				System.out.print(str.charAt(i) + " ");
+			}
+		}
+	}
+}
