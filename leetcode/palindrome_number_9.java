4d38356670171a81aa5b8082812ec0eeaa166363
Darshan L <darshanl1254@gmail.com>
2024-11-21 22:39:01 +0530

    Palindrome 9

---
+++ leetcode/palindrome_number_9.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class palindrome_number_9 {
+	static boolean checkPalindrome(int no) {
+		int reverse = 0;
+		int temp = no;
+		while (temp > 0) {
+			int rem = temp % 10;
+			reverse = (reverse * 10) + rem;
+			temp /= 10;
+		}
+		if (no == reverse) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int no = sc.nextInt();
+
+		if (checkPalindrome(no)) {
+			System.out.println(no + " is a Palindrome");
+		} else {
+			System.out.println(no + " is not a Palindrome");
+		}
+	}
+}
